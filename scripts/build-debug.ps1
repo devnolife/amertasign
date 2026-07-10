17f063703fae0a685d2ac12a6f3668c3e15fcce9
MUH. AS'AD HABIB <105841118224@student.unismuh.ac.id>
2026-07-10 16:43:10 +0800

    chore: rapikan dokumen internal, perbarui gitignore, tambah README

--- scripts/build-debug.ps1
+++
@@ -1,5 +0,0 @@
-# Build ulang APK debug dari nol dengan log ke file.
-Set-Location "D:\Coding\AmertaSign\AmertaSign-mobile\android"
-.\gradlew --stop 2>&1 | Out-Null
-.\gradlew assembleDebug 2>&1 | Set-Content "D:\Coding\AmertaSign\AmertaSign-mobile\android\build-log.txt" -Encoding UTF8
-
--- scripts/check-java.ps1
+++
@@ -1,8 +0,0 @@
-$out = @()
-$out += "=== JAVA_HOME ==="
-$out += ("JAVA_HOME=" + $env:JAVA_HOME)
-$out += "=== java -version ==="
-$java = if ($env:JAVA_HOME) { Join-Path $env:JAVA_HOME "bin\java.exe" } else { "java" }
-try { $out += (& $java -version 2>&1) } catch { $out += ("gagal: " + $_.Exception.Message) }
-$out | Set-Content "D:\Coding\AmertaSign\AmertaSign-mobile\android\java-version.txt" -Encoding UTF8
-
